9bcdc6a5fa542e1831619741f9e79c7077df690a
Simon Hyll <hyllsimon@gmail.com>
2021-08-30 13:56:08 +0200

    Added Github Pages

---
+++ .github/PAGES/layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-spacer />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Cargo Commander'
+    }
+  }
+}
+</script>
---
+++ .github/PAGES/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to Cargo Commander!
+        </v-card-title>
+        <v-card-text>
+          <p>I'll be putting more information about the project here in a while, just you wait!</p>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
